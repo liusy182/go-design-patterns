8f4e84ff8cef0e48e8d0b14415af7f2820b152ea
Liu Siyuan <siyuan.liu@autodesk.com>
2017-06-09 17:34:32 +0800

    wait and barrier

---
+++ wait/wait.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wait sync.WaitGroup
+	wait.Add(1)
+
+	go func() {
+		fmt.Println("Hello World!")
+		wait.Done()
+	}()
+	wait.Wait()
+	fmt.Println("end of main")
+}
